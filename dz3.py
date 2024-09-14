87d6b0bc3dcfe464b7905931195ebaad76c079a0
PavelSZerocod <samusenko.pasha@gmail.com>
2024-09-14 21:10:14 +0300

    add dz3

--- dz3.py
+++ dz3.py
@@ -1,3 +1,12 @@
 import requests
-import pprint
+
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+data = {'title': 'foo', 'body': 'bar', 'userId': 1}
+
+response = requests.post(url, data=data, verify=False)
+
+print(response.status_code)
+print(f"ответ на запрос - {response.json()}")
 
